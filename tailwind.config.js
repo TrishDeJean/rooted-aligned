7460d3b9d0cdf43532b4d678c4cd04b363710f57
Trish DeJean <pbartondej@gmail.com>
2026-03-19 04:00:31 +0000

    File changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -88,5 +88,18 @@ module.exports = {
   		}
   	}
   },
+  safelist: [
+    "bg-pink-100", "text-pink-700",
+    "bg-amber-100", "text-amber-700",
+    "bg-sky-100", "text-sky-700",
+    "bg-lime-100", "text-lime-700",
+    "bg-indigo-100", "text-indigo-700",
+    "bg-violet-100", "text-violet-700",
+    "bg-emerald-100", "text-emerald-700",
+    "bg-blue-100", "text-blue-700",
+    "bg-orange-100", "text-orange-700",
+    "bg-purple-100", "text-purple-700",
+    "bg-emerald-50", "border-emerald-200", "text-emerald-500", "text-emerald-700",
+  ],
   plugins: [require("tailwindcss-animate")],
 }
